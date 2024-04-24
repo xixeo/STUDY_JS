1491746c48b900f77f00b587310fa0dd4d4c8cea
xixeo <xixeo0121@gmail.com>
2024-04-24 15:14:53 +0900

    컴퓨터 vs user 주사위 게임 만들기

--- 03/03.js
+++ 03/03.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
   
   btn.addEventListener('click', () => {
     // 정수인 난수 생성
-    const random = Math.floor(Math.random() * 6) + 1;
     //console.log(random, 'random;;')
+    const random = Math.floor(Math.random() * 6) + 1;
     img.setAttribute('src', `./img/${random}.png`)
     img.setAttribute('art', `${random}`)
   });
